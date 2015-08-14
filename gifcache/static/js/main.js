07c241db3cf39e3620b4772e91846d4ca31e7536
Tyler Kershner <tylerkershner@gmail.com>
2015-08-14 17:36:32 +0000

    Some JS refinement and tweaked some CSS in the registration templates

--- gifcache/static/js/main.js
+++ gifcache/static/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 	gifIsotope();
 	tagManager();
 	showAddForm();
-	gifGrabber();	
+	gifGrabber();
 	clickOrHover();
 	copyUrl();
 	addTags();
@@ -18,7 +18,7 @@ $(document).ready(function () {
 	deleteProfile();
 	gridView();
 	partyModeToggle();
-	clickValidate();	
+	clickValidate();
 });
 
 // Fisher-Yates shuffle algorithm
@@ -37,49 +37,60 @@ function shuffle(array) {
 	return array;
 }
 
+/**
+ * jQuery.browser.mobile (http://detectmobilebrowser.com/)
+ *
+ * jQuery.browser.mobile will be true if the browser is a mobile device
+ *
+ **/
+(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+
 // Shows additional menus after clicking certain elements
 function clickMenus() {
-	$('.profile-nav-link').on('click', function() {
-		$(this).children('.profile-nav-extra').toggleClass('hidden');
-	});
-	$('.nav-links-small').on('click', function(e) {
-		var target = $(e.target);
-		if (!target.is('.small-nav-icon')) {
-			// Nothing
-		} else {
-			$(this).children('.small-nav-container').toggleClass('hidden');
-		}		
-	});
-	$('.nav-settings-icon').on('click', function(e) {
-		var target = $(e.target);
-		if (!target.is('.nav-setting-icon')) {
-			$(this).children('.nav-settings').toggleClass('hidden');
-			$(this).toggleClass('nav-settings-icon-selected');
-		} else {
-			$(this).children('.nav-settings').toggleClass('hidden');
-			$(this).toggleClass('nav-settings-icon-selected');
-		}
-	});
-	$('.inner-nav-tags').on('click', function(e) {
-		var target = $(e.target);
-		if (!target.is('.inner-nav-tags.btn')) {
-			$(this).toggleClass('purple-btn-selected');
-			$(this).children('.inner-tag-nav').toggleClass('hidden');
-		} else {
-			$(this).toggleClass('purple-btn-selected');
-			$(this).children('.inner-tag-nav').toggleClass('hidden');
-		}		
-	});
-	$('.sort').on('click', function(e) {
-		var target = $(e.target);
-		if (!target.is('.sort.btn')) {
-			$(this).toggleClass('peach-btn-selected');
-			$(this).children('.sort-options').toggleClass('hidden');
-		} else {
-			$(this).toggleClass('peach-btn-selected');
-			$(this).children('.sort-options').toggleClass('hidden');
-		}		
-	});
+	var isMobile = jQuery.browser.mobile;
+	if (isMobile) {
+    	$('.profile-nav-link').on('click', function() {
+    		$(this).children('.profile-nav-extra').toggleClass('hidden');
+    	});
+    	$('.nav-links-small').on('click', function(e) {
+    		var target = $(e.target);
+    		if (!target.is('.small-nav-icon')) {
+    			// Nothing
+    		} else {
+    			$(this).children('.small-nav-container').toggleClass('hidden');
+    		}
+    	});
+    	$('.nav-settings-icon').on('click', function(e) {
+    		var target = $(e.target);
+    		if (!target.is('.nav-setting-icon')) {
+    			$(this).children('.nav-settings').toggleClass('hidden');
+    			$(this).toggleClass('nav-settings-icon-selected');
+    		} else {
+    			$(this).children('.nav-settings').toggleClass('hidden');
+    			$(this).toggleClass('nav-settings-icon-selected');
+    		}
+    	});
+    	$('.inner-nav-tags').on('click', function(e) {
+    		var target = $(e.target);
+    		if (!target.is('.inner-nav-tags.btn')) {
+    			$(this).toggleClass('purple-btn-selected');
+    			$(this).children('.inner-tag-nav').toggleClass('hidden');
+    		} else {
+    			$(this).toggleClass('purple-btn-selected');
+    			$(this).children('.inner-tag-nav').toggleClass('hidden');
+    		}
+    	});
+    	$('.sort').on('click', function(e) {
+    		var target = $(e.target);
+    		if (!target.is('.sort.btn')) {
+    			$(this).toggleClass('peach-btn-selected');
+    			$(this).children('.sort-options').toggleClass('hidden');
+    		} else {
+    			$(this).toggleClass('peach-btn-selected');
+    			$(this).children('.sort-options').toggleClass('hidden');
+    		}
+    	});
+	}
 }
 
 // Fades in elements on the splash page
@@ -389,7 +400,7 @@ function loadingScreen() {
 				'<div class="loading"><i class="fa fa-spinner fa-pulse"></i>' +
 				'<div id="loading-blurb" class="animate">Processing, one moment please...</div></div>' +
 				'</div>';
-	$('body').append(html);	
+	$('body').append(html);
 	$('.main-loading').toggleClass('hidden');
 	setInterval(function() {
 		counter += 1
@@ -400,7 +411,7 @@ function loadingScreen() {
 		setTimeout(function() {
 			$('#loading-blurb').text(blurbs[counter]);
 			$('#loading-blurb').css('opacity', 1.0);
-		}, 800);		
+		}, 800);
 	}, 6000);
 }
 
@@ -559,7 +570,7 @@ function updateGrabberValues() {
 	valuesInput.val(finalValues);
 }
 
-// Function to hide/update element showing how many GifGrabber results have 
+// Function to hide/update element showing how many GifGrabber results have
 // been selected for addition to cache by user
 function updateSelectedGifs() {
 	var selectedCount = 0;
@@ -702,14 +713,6 @@ function gifGrabberAjax() {
 	});
 }
 
-/**
- * jQuery.browser.mobile (http://detectmobilebrowser.com/)
- *
- * jQuery.browser.mobile will be true if the browser is a mobile device
- *
- **/
-(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
-
 // Adding hover classes to elements if desktop
 function clickOrHover() {
 	var isMobile = jQuery.browser.mobile;
@@ -772,14 +775,14 @@ function hoverGifs() {
 function clickGifElements(logged_in) {
 	var isMobile = jQuery.browser.mobile;
 	$('.gif-grid-thumbnail').on('click', function() {
-		var parent = $(this).parent();		
+		var parent = $(this).parent();
 		// If mobile, hover won't work so do the image append on 'click'
 		if (isMobile) {
 			var gif = $(parent).find('.img-wrapper');
 			var thumbnail = $(parent).children('.gif-grid-thumbnail');
 			var gifUrl = $(parent).children('.display-url').val();
 			parent.toggleClass('tapped');
-			if (parent.hasClass('tapped')) {				
+			if (parent.hasClass('tapped')) {
 				// Check what kind of URL we have
 				var isGfycat = gifUrl.includes('gfycat');
 				var isGifv = gifUrl.lastIndexOf('.gifv') == gifUrl.length - '.gifv'.length;
@@ -853,7 +856,7 @@ function toggleFocus(parent, logged_in) {
 }
 
 // Scales up GIF once expand icon is clicked
-function gifExpand(parent, logged_in) {	
+function gifExpand(parent, logged_in) {
 	$(parent).find('.gif-expand').on('click', function(e) {
 		$(this).siblings('.img-wrapper').toggleClass('expanded');
 	});
@@ -1079,8 +1082,8 @@ function gridView() {
 			totalGifs += 1;
 		});
 		var html = '<div class="lightbox grid-wrapper">' +
-					'<div class="lightbox-tag-title lobster">' + tagTitle + '<div class="lightbox-gif-count">' + totalGifs + ' GIFs</div></div>' +					
-					'<div id="grid-cancel" class="lightbox-control lightbox-cancel animate-fast"><i class="fa fa-times"></i></div>' + 
+					'<div class="lightbox-tag-title lobster">' + tagTitle + '<div class="lightbox-gif-count">' + totalGifs + ' GIFs</div></div>' +
+					'<div id="grid-cancel" class="lightbox-control lightbox-cancel animate-fast"><i class="fa fa-times"></i></div>' +
 					'<div class="lightbox-control lightbox-cancel grid-menu-icon animate-fast"><i class="fa fa-bars"></i>' +
 					'<div class="grid-controls hidden"><div class="grid-size-slider lightbox-control animate-fast">' +
 					'<input id="grid-slider" type="range" value="10" min="10" max="50" step="10"><div class="lightbox-control-label">GIF Size</div>' +
@@ -1388,7 +1391,7 @@ function validationResults(data) {
 	if (data['dupes'].length > 0 || data['404s'].length > 0) {
 		$('.validation-container').addClass('validation-expanded');
 	}
-	var validationTitle = '<div class="validation-title">Validate <div>Cache</div></div><div class="validation-results"></div>';	
+	var validationTitle = '<div class="validation-title">Validate <div>Cache</div></div><div class="validation-results"></div>';
 	$('.validation-container').prepend(validationTitle);
 	$('#validation-loading').remove();
 	$('.validation-wrapper').on('click', function(e) {
@@ -1398,7 +1401,7 @@ function validationResults(data) {
 		} else {
 			validationTeardown();
 		}
-	});	
+	});
 	// Dupe logic
 	var dupes = data['dupes'];
 	if (dupes.length > 0) {
@@ -1424,8 +1427,8 @@ function validationResults(data) {
 				var tagsElement = '';
 			}
 			var element = '<div class="validation-result animate-fast"><div class="validation-result-inner-wrapper hidden"></div>' +
-						'<div class="validation-id hidden">' + dupes[i][0] + 
-						'</div>' + image + '<div class="validation-label">' + dupes[i][2] + 
+						'<div class="validation-id hidden">' + dupes[i][0] +
+						'</div>' + image + '<div class="validation-label">' + dupes[i][2] +
 						'</div>' + tagsElement + '</div>';
 			dupesHtml += element
 		}
