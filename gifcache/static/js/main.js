f98944c33ccb4009294bd3539b6b146c96097bcf
Tyler Kershner <tylerkershner@gmail.com>
2015-08-14 10:54:38 -0400

    Sme JS usability fixes and CSS style tweaks

--- gifcache/static/js/main.js
+++ gifcache/static/js/main.js
@@ -21,6 +21,22 @@ $(document).ready(function () {
 	clickValidate();	
 });
 
+// Fisher-Yates shuffle algorithm
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
+}
+
 // Shows additional menus after clicking certain elements
 function clickMenus() {
 	$('.profile-nav-link').on('click', function() {
@@ -36,8 +52,9 @@ function clickMenus() {
 	});
 	$('.nav-settings-icon').on('click', function(e) {
 		var target = $(e.target);
-		if (!target.is('i.fa.fa-cog')) {
-			// Nothing
+		if (!target.is('.nav-setting-icon')) {
+			$(this).children('.nav-settings').toggleClass('hidden');
+			$(this).toggleClass('nav-settings-icon-selected');
 		} else {
 			$(this).children('.nav-settings').toggleClass('hidden');
 			$(this).toggleClass('nav-settings-icon-selected');
@@ -46,7 +63,8 @@ function clickMenus() {
 	$('.inner-nav-tags').on('click', function(e) {
 		var target = $(e.target);
 		if (!target.is('.inner-nav-tags.btn')) {
-			// Nothing
+			$(this).toggleClass('purple-btn-selected');
+			$(this).children('.inner-tag-nav').toggleClass('hidden');
 		} else {
 			$(this).toggleClass('purple-btn-selected');
 			$(this).children('.inner-tag-nav').toggleClass('hidden');
@@ -55,28 +73,13 @@ function clickMenus() {
 	$('.sort').on('click', function(e) {
 		var target = $(e.target);
 		if (!target.is('.sort.btn')) {
-			// Nothing
+			$(this).toggleClass('peach-btn-selected');
+			$(this).children('.sort-options').toggleClass('hidden');
 		} else {
 			$(this).toggleClass('peach-btn-selected');
 			$(this).children('.sort-options').toggleClass('hidden');
 		}		
 	});
-}
-
-// Standard Fisher-Yates shuffle algorithm
-function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
-		// And swap it with the current element.
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-	return array;
 }
 
 // Fades in elements on the splash page
@@ -169,6 +172,7 @@ function colorMainForm() {
 	});
 }
 
+// Colors elements of the profile view page
 function colorProfile() {
 	var randomnumber = (Math.random() * (COLORS.length - 0 + 1) ) << 0
 	var counter = randomnumber;
@@ -207,6 +211,7 @@ function colorProfile() {
 	});
 }
 
+// Called when the 'shufle colors' button is clicked
 function reColorProfile() {
 	$('.color-btn').on('click', function() {
 		colorProfile();
@@ -269,7 +274,6 @@ function gifIsotope() {
 		$('.sort-date, .tag-sort-date').removeClass('blue-btn-selected');
 		grid.isotope('shuffle');
 	});
-
 	// Grid of tag groups and sorting options
 	var taggedGrid = $('#tagged-gif-grid').isotope({
 		itemSelector: '.tag-group',
@@ -372,6 +376,7 @@ function showAddForm() {
 	});
 }
 
+// Reusable full-screen loading animation
 function loadingScreen() {
 	var counter = 1;
 	var blurbs = [
@@ -463,7 +468,6 @@ function gifGrabberSuggestions() {
 	var choice1 = '<div class="suggestion animate-fast">' + choices[0] + '</div>';
 	var choice2 = '<div class="suggestion animate-fast">' + choices[1] + '</div>';
 	var choice3 = '<div class="suggestion animate-fast">' + choices[2] + '</div>';
-	var containerEnd = '</div></div>';
 	var html = choice1 + choice2 + choice3;
 	$('.suggestions').fadeOut('slow', function() {
 		$(this).empty();
@@ -555,7 +559,8 @@ function updateGrabberValues() {
 	valuesInput.val(finalValues);
 }
 
-// Function to hide/update element showing how many GifGrabber results have been selected for addition to cache by user
+// Function to hide/update element showing how many GifGrabber results have 
+// been selected for addition to cache by user
 function updateSelectedGifs() {
 	var selectedCount = 0;
 	$('.grabber-results-element-inner').each(function() {
@@ -705,13 +710,14 @@ function gifGrabberAjax() {
  **/
 (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
-// Determining which Gif interaction method to use based on device
+// Adding hover classes to elements if desktop
 function clickOrHover() {
 	var isMobile = jQuery.browser.mobile;
 	if (isMobile) {
 		// Nothing
 	} else {
 		hoverGifs();
+		$('.inner-nav-tags, .sort, .nav-settings-icon, .profile-nav-link').addClass('desktop-hover');
 	}
 }
 
@@ -806,8 +812,8 @@ function clickGifElements(logged_in) {
 	});
 }
 
+// Handles coloring/decoloring elements after 'focus' class is applied
 function toggleFocus(parent, logged_in) {
-	console.log('TOGGLING FOCUS');
 	// Adding some extra positoning for logged-in menus
 	if (logged_in === 'True') {
 		parent.toggleClass('focused');
@@ -1066,29 +1072,38 @@ function deleteProfile() {
 // Setup/Teardown of grid view
 function gridView() {
 	$('.display-grid-icon').on('click', function() {
+		var isMobile = jQuery.browser.mobile;
 		var tagTitle = $(this).parents('.tag-group').children('.tag-title').text();
 		var totalGifs = 0;
 		$(this).parents('.tag-group').children('.gif-grid-element').each(function() {
 			totalGifs += 1;
 		});
 		var html = '<div class="lightbox grid-wrapper">' +
-					'<div class="lightbox-tag-title lobster">' + tagTitle + '<div class="lightbox-gif-count">' + totalGifs + ' GIFs</div></div>' +
-					'<div class="lightbox-control lightbox-cancel animate-fast">' +
-					'<i class="fa fa-times"></i></div><div class="grid-controls">' +
-					'<div class="grid-size-slider lightbox-control animate-fast"><input id="grid-slider" type="range" value="10" min="10" max="50" step="10">' +
-					'<div class="lightbox-control-label">GIF Size</div></div>' +
-					'<div class="grid-refresh lightbox-control animate-fast"><i class="fa fa-refresh"></i><div class="lightbox-control-label">Refresh Grid</div></div></div>' +
+					'<div class="lightbox-tag-title lobster">' + tagTitle + '<div class="lightbox-gif-count">' + totalGifs + ' GIFs</div></div>' +					
+					'<div id="grid-cancel" class="lightbox-control lightbox-cancel animate-fast"><i class="fa fa-times"></i></div>' + 
+					'<div class="lightbox-control lightbox-cancel grid-menu-icon animate-fast"><i class="fa fa-bars"></i>' +
+					'<div class="grid-controls hidden"><div class="grid-size-slider lightbox-control animate-fast">' +
+					'<input id="grid-slider" type="range" value="10" min="10" max="50" step="10"><div class="lightbox-control-label">GIF Size</div>' +
+					'</div><div class="grid-refresh lightbox-control animate-fast"><i class="fa fa-refresh"></i>' +
+					'<div class="lightbox-control-label">Refresh Grid</div></div></div></div>' +
 					'<div class="lightbox-title pulse"><div class="lobster">Grid</div><div class="pt">View</div></div>' +
 					'<div class="grid-view-wrapper"><div class="grid-view-container"></div></div>' +
 					'</div>';
 		$('body').append(html);
+		if (isMobile) {
+			$('.grid-menu-icon').on('click', function() {
+				$('.grid-controls').toggleClass('hidden');
+			});
+		} else {
+			$('.grid-menu-icon').addClass('desktop-hover');
+		}
 		var randomnumber = (Math.random() * (COLORS.length - 0 + 1) ) << 0
 		var randomnumber1 = (Math.random() * (COLORS.length - 0 + 1) ) << 0
 		$('.lightbox-title .lobster').css('color', COLORS[randomnumber]);
 		$('.lightbox-tag-title').css('color', COLORS[randomnumber1]);
 		$('.lightbox-gif-count').css('color', 'white');
 		populateGrid($(this));
-		$('.lightbox-cancel').on('click', function() {
+		$('#grid-cancel').on('click', function() {
 			$('.lightbox').remove();
 		});
 	});
@@ -1129,16 +1144,16 @@ function populateGrid(icon) {
 			isFitWidth: true
 		}
 	});
+	grid.isotope('layout');
 	gridSlider(grid);
 	gridRefresh(grid);
 }
 
+// Handles updating image CSS attributes when slider is engaged
 function gridSlider(grid) {
 	$('#grid-slider').on('input', function() {
 		var scaleValue = $(this).val();
-		var imgGrid = $(this).parent().siblings('.grid-view-wrapper').children('.grid-view-container');
-		var imgGrid = $(this).parents('.grid-controls').siblings('.grid-view-wrapper').children('.grid-view-container');
-		console.log(imgGrid);
+		var imgGrid = $(this).parents('.grid-menu-icon').siblings('.grid-view-wrapper').children('.grid-view-container');
 		imgGrid.children('.grid-img-wrapper').each(function() {
 			$(this).css({
 				'width': scaleValue + 'em'
@@ -1150,6 +1165,7 @@ function gridSlider(grid) {
 	});
 }
 
+// Relayout grid view grid
 function gridRefresh(grid) {
 	$('.grid-refresh').on('click', function() {
 		grid.isotope('layout');
@@ -1235,9 +1251,10 @@ function partyMode(tagGroup) {
 }
 
 // Quick function to get random element from jQuery collection
-$.fn.random = function() {
-	return this.eq(Math.floor(Math.random() * this.length));
-}
+// Possibly not needed anymore, commenting out until problem arises
+// $.fn.random = function() {
+// 	return this.eq(Math.floor(Math.random() * this.length));
+// }
 
 // Function to randomly 'focus' on a GIF in party mode
 function autoFocusGif() {
@@ -1289,10 +1306,8 @@ function makeNewPosition($content) {
 	// Get viewport dimensions (remove the dimension of the div)
 	var h = $content.height() - 400;
 	var w = $content.width() - 300;
-
 	var nh = Math.floor(Math.random() * h);
 	var nw = Math.floor(Math.random() * w);
-
 	return [nh, nw];
 }
 
@@ -1302,7 +1317,6 @@ function calcSpeed(prev, next) {
 	var greatest = x > y ? x : y;
 	var speedModifier = 0.1;
 	var speed = Math.ceil(greatest / speedModifier);
-
 	return speed;
 }
 
@@ -1310,7 +1324,6 @@ function animateDiv($target) {
 	var newq = makeNewPosition($target.parent());
 	var oldq = $target.position();
 	var speed = calcSpeed([oldq.top, oldq.left], newq);
-
 	$target.animate({
 		top: newq[0],
 		left: newq[1]
@@ -1347,6 +1360,7 @@ function clickValidate() {
 	});
 }
 
+// Creates container div for validation check
 function validationSetup() {
 	var html = 	'<div id="validation-loading" class="lightbox">' +
 				'<div class="loading"><i class="fa fa-spinner fa-pulse"></i>' +
@@ -1362,12 +1376,14 @@ function validationSetup() {
 	});
 }
 
+// Removes validation container
 function validationTeardown() {
 	$('.validation-wrapper').addClass('hidden');
 	$('.validation-title, .validation-results').remove();
 	$('#validate-submit').addClass('hidden');
 }
 
+// Creates container div and populates it with results from AJAX call
 function validationResults(data) {
 	if (data['dupes'].length > 0 || data['404s'].length > 0) {
 		$('.validation-container').addClass('validation-expanded');
@@ -1460,6 +1476,7 @@ function validationResults(data) {
 	});
 }
 
+// Upates hidden input with IDs for GIFs to be deleted
 function updateValidateValues() {
 	var valuesInput = $('#validation-values');
 	valuesInput.val('');
