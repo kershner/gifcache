1e54317433a631a82852282ae5db033515181a8e
Tyler Kershner <tylerkershner@gmail.com>
2015-08-24 19:07:55 +0000

    Fix for small JS bug I just introduced

--- gifcache/static/js/main.js
+++ gifcache/static/js/main.js
@@ -48,7 +48,7 @@ function shuffle(array) {
 // Function to lighten/darken HEX color by percentage
 // Credit to StackOverflow user Pimp Trizkit
 // http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
-function shadeColor2(color, percent) {   
+function shadeColor2(color, percent) {
     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
 }
@@ -155,13 +155,17 @@ function colorPageElements() {
 		if (counter > HOME_COLORS.length - 1 ) {
 			counter = 0;
 		}
-		var homeColor = HOME_COLORS[counter];		
-		if (numLoops === 1) {
+		var homeColor = HOME_COLORS[counter];
+		if ($(this).attr('id') === 'landing-page') {
 			colorChoice = homeColor;
 		}
 		else if (numLoops >= 2) {
 			secondColor = homeColor;
 			$(this).css({
+				'background-color': homeColor
+			});
+		} else {
+		    $(this).css({
 				'background-color': homeColor
 			});
 		}
@@ -942,7 +946,7 @@ function bulkOperations() {
 			} else {
 				loadingScreen();
 				$(this).parent().submit();
-			}					
+			}
 		});
 	});
 	var html = '<div class="bulk-selected-gifs pulse hidden">' +
