a9e03b62de53a80f37172db8c97d063710c06355
Tyler Kershner <tylerkershner@gmail.com>
2015-09-23 16:40:33 -0400

    Added more tests

--- gifcache/apps/users/tests.py
+++ gifcache/apps/users/tests.py
@@ -1,7 +1,9 @@
+from .views import scrape_reddit, validate_cache, get_nav_gifs, which_element
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.test import TestCase
 from .models import Gif
+import os
 
 
 # Create your tests here.
@@ -9,6 +11,19 @@ class TestUserViews(TestCase):
     """
     Testing very basic profile views to see if they return HTTP 200
     """
+    def setUp(self):
+        # Create fake user, fake gif with fake tag
+        self.user = User.objects.create_user(username='test',
+                                             email='test@test.com',
+                                             password='test_password')
+        self.gif = Gif(owner=self.user, url='test_gif.gif',
+                       display_url='test_gif.gif',
+                       created=timezone.now(),
+                       thumbnail='test_thumbnail.jpg',
+                       label='Test Gif!')
+        self.gif.save()
+        self.gif.tags.add('test')
+
     def test_profile_view(self):
         response = self.client.get('/u/kershner25')
         self.assertEqual(response.status_code, 200)
@@ -18,29 +33,108 @@ class TestUserViews(TestCase):
         response = self.client.get('/u/kershner25/edit-profile')
         self.assertEqual(response.status_code, 301)
 
-    # def test_edit_profile_view(self):
-    #     # Create fake user, test that we can get to their edit-profile page
-    #     user = User.objects.create_user('test', 'test@test.com', 'test_password')
-    #     response = self.client.get('/u/%s/edit-profile' % user.username)
-    #     print '#########################'
-    #     print response.content
-    #     self.assertEqual(response.status_code, 200)
-
     def test_share_tag_view(self):
-        # Create fake user, fake gif with fake tag, test the tag view
-        user = User.objects.create_user('test', 'test@test.com', 'test_password')
-        gif = Gif(owner=user, url='test_gif.gif', display_url='test_gif.gif', created=timezone.now(),
-                  thumbnail='test_thumbnail.jpg', label='Test Gif!')
-        gif.save()
-        gif.tags.add('test')
-        response = self.client.get('/u/%s/tags/%s' % (user.username, 'test'))
+        response = self.client.get('/u/%s/tags/%s' % (self.user.username, 'test'))
         self.assertEqual(response.status_code, 200)
 
     def test_share_gif_view(self):
-        # Create fake user, fake gif, test the gif view
-        user = User.objects.create_user('test', 'test@test.com', 'test_password')
-        gif = Gif(owner=user, url='test_gif.gif', display_url='test_gif.gif', created=timezone.now(),
-                  thumbnail='test_thumbnail.jpg', label='Test Gif!')
-        gif.save()
-        response = self.client.get('/u/%s/gifs/%d' % (user.username, gif.id))
+        response = self.client.get('/u/%s/gifs/%d' % (self.user.username, self.gif.id))
         self.assertEqual(response.status_code, 200)
+
+
+class TestCustomFunctions(TestCase):
+    """
+    Testing custom functions used throughout GifCache.com
+    """
+    def setUp(self):
+        # Create fake user, fake gif with fake tag
+        self.user = User.objects.create_user(username='test',
+                                             email='test@test.com',
+                                             password='test_password')
+
+    def test_reddit_scraper(self):
+        results = scrape_reddit('gifs', 'hot')
+        test_case = False
+        if len(results) > 1:
+            test_case = True
+        self.assertEqual(test_case, True)
+
+    def test_validate_success(self):
+        # Checking if validate function successfully validates known gifs
+        path = 'http://www.gifcache.com/static/img/homegif'
+        for x in xrange(5):
+            temp_gif = Gif(owner=self.user,
+                           url='%s%d.gif' % (path, x),
+                           display_url='%d.gif' % x,
+                           created=timezone.now(),
+                           thumbnail='test_thumbnail%d.jpg' % x,
+                           label='Test Gif %d!' % x)
+            temp_gif.save()
+        results = validate_cache(self.user.username)
+        test_case = False
+        if not results['404s']:
+            test_case = True
+        self.assertEqual(test_case, True)
+
+    def test_validate_fail(self):
+        # Checking if validate function successfully catches 404'd gifs
+        temp_gif = Gif(owner=self.user,
+                       url='http://www.kershner.org/not-a-gif.gif',
+                       display_url='not-a-gif.gif',
+                       created=timezone.now(),
+                       thumbnail='not-a-thumbnail.jpg',
+                       label='Not a Gif!')
+        temp_gif.save()
+        results = validate_cache(self.user.username)
+        test_case = False
+        if results['404s']:
+            test_case = True
+        self.assertEqual(test_case, True)
+
+    def test_validate_dupes(self):
+        # Checking if validate function successfully catches duped gifs
+        temp_gif_1 = Gif(owner=self.user,
+                         url='http://www.gifcache.com/static/img/homegif0.gif',
+                         display_url='testgif1.gif',
+                         created=timezone.now(),
+                         thumbnail='testgif1.jpg',
+                         label='Test Gif 1!')
+        temp_gif_2 = Gif(owner=self.user,
+                         url='http://www.gifcache.com/static/img/homegif0.gif',
+                         display_url='testgif2.gif',
+                         created=timezone.now(),
+                         thumbnail='testgif2.gif',
+                         label='Test Gif 2!')
+        temp_gif_1.save()
+        temp_gif_2.save()
+        results = validate_cache(self.user.username)
+        test_case = False
+        if results['dupes']:
+            test_case = True
+        self.assertEqual(test_case, True)
+
+    def test_nav_gif(self):
+        # Test to see if this function returns correct number of 'nav gifs'
+        gif_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'static/img/')
+        files = []
+        for (dirpath, dirnames, filenames) in os.walk(gif_dir):
+            files.extend(filenames)
+            break
+        test_number = len([f for f in files if f.startswith('navgif')])
+        self.assertEqual(get_nav_gifs(), test_number)
+
+    def test_which_element_success(self):
+        test_url_1 = 'http://www.gif.com/gif.mp4'
+        test_url_2 = 'http://www.gif.com/gif.gif'
+        test_url_3 = 'http://www.gfycat.com/gfyname'
+        test_url_4 = 'http://www.gif.com/gif.gifv'
+        test_case = True
+        if which_element(test_url_1) != 'video':
+            test_case = False
+        if which_element(test_url_2) != 'img':
+            test_case = False
+        if which_element(test_url_3) != 'gfycat':
+            test_case = False
+        if which_element(test_url_4) != 'gifv':
+            test_case = False
+        self.assertEqual(test_case, True)
